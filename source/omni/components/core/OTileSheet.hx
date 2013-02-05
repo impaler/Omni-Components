c085c2563af39a2f2e54a4497779448d72f98d3f
impaler <user123@server.fake>
2013-02-05 19:48:46 +1030

    A basic drawTiles brush

---
+++ source/omni/components/core/OTileSheet.hx
@@ -0,0 +1,78 @@
+package omni.components.core;
+
+import nme.Assets;
+import nme.geom.Point;
+import nme.geom.Rectangle;
+import omni.components.core.interfaces.IDestroyable;
+
+import nme.display.Tilesheet;
+import nme.display.BitmapData;
+
+class OTileSheet implements IDestroyable
+{
+
+	public var tileSheets:Hash<Tilesheet>;
+
+	public var lastTileID:Int = 0;
+
+	public static var instance(getInstance, null):OTileSheet;
+
+	private static function getInstance():OTileSheet
+	{
+		if(instance == null)
+		{
+			instance = new OTileSheet();
+		}
+		return instance;
+	}
+
+	public function new()
+	{
+		tileSheets = new Hash();
+	}
+
+	public function add(id:String, path:String):Void
+	{
+		var bitmap = Assets.getBitmapData(path);
+		var tileSheet = new Tilesheet(bitmap);
+		tileSheets.set(id, tileSheet);
+	}
+
+	public function addTileRect(sheetID:String, rectangle:Rectangle, centerPoint:Point = null):Int
+	{
+		var tileSheet = get(sheetID);
+		tileSheet.addTileRect(rectangle, centerPoint);
+		var id = lastTileID;
+		lastTileID ++;
+
+		return id;
+	}
+
+	public function get(id:String):Tilesheet
+	{
+		return tileSheets.get(id);
+	}
+
+	public function remove(id:String):Void
+	{
+		tileSheets.remove(id);
+	}
+
+	public function destroy():Void
+	{
+		tileSheets = null;
+	}
+
+}
+
+class TileDef
+{
+
+	public var sheetID:String;
+	public var tileID:Float;
+	public var rect:Rectangle;
+
+	public function new():Void
+	{
+	}
+}
---
+++ source/omni/components/style/brush/BrushDrawTiles.hx
@@ -0,0 +1,56 @@
+package omni.components.style.brush;
+
+import nme.geom.Point;
+import nme.geom.Rectangle;
+import omni.components.core.OTileSheet;
+import nme.display.Tilesheet;
+import omni.components.core.interfaces.IOComponent;
+import omni.components.core.interfaces.IBrush;
+
+import nme.display.Sprite;
+import nme.display.Graphics;
+
+class BrushDrawTiles implements IBrush
+{
+	public var sheetID:String;
+	public var statePositions:Hash<TileDef>;
+	
+	public function new()
+	{
+		statePositions = new Hash();
+	}
+	
+	public function addTile(state:String, rect:Rectangle, sheetID:String):Void
+	{
+		var tileID = OTileSheet.instance.addTileRect(sheetID, rect);
+		
+		var tile = new TileDef();
+		tile.sheetID = sheetID;
+		tile.tileID = tileID;
+		tile.rect = rect;
+		
+		statePositions.set(state, tile);
+	}
+
+	public function update(drawTarget:IOComponent)
+	{
+		var TileData = statePositions.get(drawTarget.state);
+		var graphicsTarget:Graphics = drawTarget.sprite.graphics;
+		graphicsTarget.clear();
+		
+		var tileID = TileData.tileID;
+		var tileX = 0;
+		var tileY = 0;
+		var tileData:Array<Float>;
+		tileData = [tileX, tileY, tileID];
+		  
+		var sheet = OTileSheet.instance.get(TileData.sheetID);
+		sheet.drawTiles(graphicsTarget, tileData);
+	}
+
+	public function destroy():Void
+	{
+		statePositions = null;
+	}
+
+}
