6f9fdb30c1c8e223318e239193a9e93c7c75d577
impaler <user123@server.fake>
2013-02-14 22:51:49 +1030

    use temp value, formatting and button styles

--- source/omni/components/gui/controls/ScrollBar.hx
+++ source/omni/components/gui/controls/ScrollBar.hx
@@ -1,8 +1,11 @@
 package omni.components.gui.controls;
 
+import omni.components.style.OBackgroundStyle;
+import omni.components.core.OButtonBase;
 import omni.components.core.signals.OSignalMouse;
 import omni.components.core.interfaces.IStyle;
-import omni.components.gui.layout.Layout;
+import omni.components.core.OLayout;
+import omni.components.gui.controls.ScrollBarButton;
 import nme.geom.Rectangle;
 import omni.components.gui.controls.Slider.SliderBaseStyle;
 import flash.display.DisplayObjectContainer;
@@ -20,41 +23,41 @@ class ScrollBar extends Slider
 
 	public var _working:Bool = true;
 
-	//***********************************************************
-	//                  Event Handlers
-	//***********************************************************
+//***********************************************************
+//                  Event Handlers
+//***********************************************************
 
-	override public function handleMouseMove(?e:OSignalMouse):Void
+	override public function handleMouseMove( ?e:OSignalMouse ):Void
 	{
-		super.handleMouseMove(e);
+		super.handleMouseMove( e );
 	}
 
-	override public function handleMouseDown(?e:OSignalMouse):Void
+	override public function handleMouseDown( ?e:OSignalMouse ):Void
 	{
-		button.handleMouseDown(e);
+		button.handleMouseDown( e );
 
-		updateButtonLocationFromDown();
-		updateValueFromButtonLocation();
+		updateButtonLocationFromDown( );
+		updateValueFromButtonLocation( );
 
-		button.startDrag(false, _rect);
-		mouseMove.add(handleMouseMove);
-		mouseUp.add(handleMouseUp);
+		button.startDrag( false, _rect );
+		mouseMove.add( handleMouseMove );
+		mouseUp.add( handleMouseUp );
 	}
 
-	override public function handleMouseUp(?e:OSignalMouse):Void
+	override public function handleMouseUp( ?e:OSignalMouse ):Void
 	{
-		button.handleMouseUp(e);
-		button.stopDrag();
+		button.handleMouseUp( e );
+		button.stopDrag( );
 
-		updateValueOnMouseMove();
+		updateValueOnMouseMove( );
 
-		mouseUp.remove(handleMouseUp);
-		mouseMove.remove(handleMouseMove);
+		mouseUp.remove( handleMouseUp );
+		mouseMove.remove( handleMouseMove );
 	}
 
-	override public function handleMouseWheel(?e:OSignalMouse):Void
+	override public function handleMouseWheel( ?e:OSignalMouse ):Void
 	{
-		if(_type == Layout.HORIZONTALLY)
+		if( _type == OLayout.HORIZONTALLY )
 		{
 			value += e.delta > 0 ? step : - step;
 		}
@@ -62,35 +65,36 @@ class ScrollBar extends Slider
 		{
 			value -= e.delta > 0 ? step : - step;
 		}
-		e.updateAfterEvent();
+		e.updateAfterEvent( );
 	}
 
-	//***********************************************************
-	//                  Component Methods
-	//***********************************************************
+//***********************************************************
+//                  Component Methods
+//***********************************************************
 
-	override public function refreshButton():Void
+	override public function refreshButton( ):Void
 	{
-		_max = Math.max(contentSize - pageSize, 0);
+		_max = Math.max( contentSize - pageSize, 0 );
 
-		if(_max > 0)
+		if( _max > 0 )
 		{
 			barNeeded = true;
-			if(_type == Slider.HORIZONTALLY)
+
+			if( _type == Slider.HORIZONTALLY )
 			{
-				_realSize = Std.int((pageSize * _width) / (_max + pageSize));
-				button._size(Math.max(_realSize, _minSize), _height);
-				_rect.width = Math.ceil(_width - button.width);
+				_realSize = Std.int( (pageSize * _width) / (_max + pageSize) );
+				button._size( Math.max( _realSize, _minSize ), _height );
+				_rect.width = Math.ceil( _width - button.width );
 				button.x = _value / _max * _rect.width;
-				if(button.x + button.width > _width) button.x = _width - button.width;
+				if( button.x + button.width > _width ) button.x = _width - button.width;
 			}
 			else
 			{
-				_realSize = Std.int((pageSize * _height) / (_max + pageSize));
-				button._size(_width, Math.max(_realSize, _minSize));
-				_rect.height = Math.ceil(_height - button.height);
+				_realSize = Std.int( (pageSize * _height) / (_max + pageSize) );
+				button._size( _width, Math.max( _realSize, _minSize ) );
+				_rect.height = Math.ceil( _height - button.height );
 				button.y = _value / _max * _rect.height;
-				if(button.y + button.height > _height) button.y = _height - button.height;
+				if( button.y + button.height > _height ) button.y = _height - button.height;
 			}
 			_working = true;
 		}
@@ -100,111 +104,110 @@ class ScrollBar extends Slider
 			_value = 0;
 			_rect.width = 0;
 			_rect.height = 0;
-			button.move(0, 0);
-			button._size(0, 0);
+			button.move( 0, 0 );
+			button._size( 0, 0 );
 			_working = false;
 		}
 	}
 
-	override public function updateValueOnMouseMove():Void
+	override public function updateValueOnMouseMove( ):Void
 	{
-		var valueChange = _value;
+		tempValue = _value;
 
-		if(_type == Slider.HORIZONTALLY)
+		if( _type == Slider.HORIZONTALLY )
 		{
-			valueChange = clamp(Math.ceil(button.sprite.x / _rect.width * _max));
+			tempValue = clamp( Math.ceil( button.sprite.x / _rect.width * _max ) );
 		}
 		else
 		{
-			valueChange = clamp(Math.ceil(button.sprite.y / _rect.height * _max));
+			tempValue = clamp( Math.ceil( button.sprite.y / _rect.height * _max ) );
 		}
 
-		if(_value != valueChange)
+		if( _value != tempValue )
 		{
-			_value = valueChange;
-			onChange.dispatch(_value);
+			_value = tempValue;
+			onChange.dispatch( _value );
 		}
 	}
 
-	override public function updateValueFromButtonLocation():Void
+	override public function updateValueFromButtonLocation( ):Void
 	{
-		var valueChange = _value;
-		if(_type == Slider.HORIZONTALLY)
+		tempValue = _value;
+
+		if( _type == Slider.HORIZONTALLY )
 		{
-			valueChange = Math.ceil(button.x / _rect.width * _max);
+			tempValue = Math.ceil( button.x / _rect.width * _max );
 		}
 		else
 		{
-			valueChange = Math.ceil(button.y / _rect.height * _max);
+			tempValue = Math.ceil( button.y / _rect.height * _max );
 		}
-		if(_value != valueChange)
+		if( _value != tempValue )
 		{
-			_value = valueChange;
-			onChange.dispatch(_value);
+			_value = tempValue;
+			onChange.dispatch( _value );
 		}
 	}
 
 	public var barNeeded(getBarNeeded, setBarNeeded):Bool;
 
-	public function setBarNeeded(value:Bool):Bool
+	public function setBarNeeded( value:Bool ):Bool
 	{
 		barNeeded = value;
 		return barNeeded;
 	}
 
-	public function getBarNeeded():Bool
+	public function getBarNeeded( ):Bool
 	{
-		_max = Math.max(contentSize - pageSize, 0);
+		_max = Math.max( contentSize - pageSize, 0 );
 
-		if(_max > 0)
+		if( _max > 0 )
 		{
 			return true;
-
 		}
 		else
 		{
-
 			return false;
 		}
 	}
 
-	public function setContentSize(value:Int):Int
+	public function setContentSize( value:Int ):Int
 	{
-		if(contentSize != value)
+		if( contentSize != value )
 		{
 			contentSize = value;
 		}
 		return value;
 	}
 
-	public function setPageSize(value:Int):Int
+	public function setPageSize( value:Int ):Int
 	{
-		if(pageSize != value)
+		if( pageSize != value )
 		{
 			pageSize = value;
 		}
 		return value;
 	}
 
-	public function setMinSize(value:Int):Int
+	public function setMinSize( value:Int ):Int
 	{
-		if(_minSize != value)
+		if( _minSize != value )
 		{
 			_minSize = value;
 		}
 		return _minSize;
 	}
 
-	public function getMinSize():Int
+	public function getMinSize( ):Int
 	{
 		return _minSize;
 	}
 
-	//***********************************************************
-	//                  Component Style
-	//***********************************************************
+//***********************************************************
+//                  Component Style
+//***********************************************************
 
-	override public function getStyleId():String
+	override public function getStyleId( ):String
 	{
 		return ScrollBarStyle.styleString;
 	}
@@ -214,15 +217,18 @@ class ScrollBar extends Slider
 class ScrollBarStyle extends SliderBaseStyle
 {
 
+	public var decreaseButtonStyle:ScrollBarButtonStyle;
+	public var increaseButtonStyle:ScrollBarButtonStyle;
+
+	public var scrollButtons:Bool;
+
 	public static var styleString:String = "ScrollBarStyle";
 
-	public function new()
+	public function new( )
 	{
-		super();
+		super( );
+		scrollButtons = false;
 		styleID = styleString;
 	}
+
 }
-
-
-
-
