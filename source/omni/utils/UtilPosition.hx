bb9db016a102bfb268bdca8aa10513a04c3ac43c
impaler <user123@server.fake>
2013-02-21 11:29:43 +1030

    debug lines

--- source/omni/utils/UtilPosition.hx
+++ source/omni/utils/UtilPosition.hx
@@ -1,5 +1,6 @@
 package omni.utils;
 
+import nme.display.Sprite;
 import omni.components.core.interfaces.IOComponent;
 import nme.display.DisplayObject;
 
@@ -44,6 +45,48 @@ class UtilPosition
 	public static inline function HAlignDisplayObjectToOther( moving:DisplayObject, target:DisplayObject ):Void
 	{
 		moving.x = (target.width * .5) - (moving.width * .5);
+	}
+
+	public static inline function setRegistrationPoint( parent:Sprite, regx:Float, regy:Float, color:Float = 0xffffff,
+	                                                    line:Float = .2
+	):Void
+	{
+
+		var markReg:Sprite = new Sprite();
+		markReg.graphics.clear( );
+		markReg.graphics.lineStyle( line, color );
+		markReg.graphics.moveTo( - 5, - 5 );
+		markReg.graphics.lineTo( 5, 5 );
+		markReg.graphics.moveTo( - 5, 5 );
+		markReg.graphics.lineTo( 5, - 5 );
+		markReg.graphics.endFill( );
+		parent.addChild( markReg );
+		markReg.x = regx;
+		markReg.y = regy;
+	}
+
+	public static inline function drawYRegLine( parent:Sprite, regy:Float, color:Int = 0xffffff, line:Float = .2
+	):Void
+	{
+		var markReg:Sprite = new Sprite();
+		markReg.graphics.clear( );
+		markReg.graphics.lineStyle( line, color );
+		markReg.graphics.moveTo( 0, regy );
+		markReg.graphics.lineTo( nme.Lib.stage.stageWidth, regy );
+		markReg.graphics.endFill( );
+		parent.addChild( markReg );
+	}
+
+	public static inline function drawXRegLine( parent:Sprite, regx:Float, color:Int = 0xffffff, line:Float = .2
+	):Void
+	{
+		var markReg:Sprite = new Sprite();
+		markReg.graphics.clear( );
+		markReg.graphics.lineStyle( line, color );
+		markReg.graphics.moveTo( regx, 0 );
+		markReg.graphics.lineTo( regx, nme.Lib.stage.stageHeight );
+		markReg.graphics.endFill( );
+		parent.addChild( markReg );
 	}
 
 }
