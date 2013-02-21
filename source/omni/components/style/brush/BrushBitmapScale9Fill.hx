54d7813f246e461a1a662f770a8dec30f7328550
impaler <user123@server.fake>
2013-02-21 11:31:58 +1030

    debug paramater

--- source/omni/components/style/brush/BrushBitmapScale9Fill.hx
+++ source/omni/components/style/brush/BrushBitmapScale9Fill.hx
@@ -9,17 +9,19 @@ class BrushBitmapScale9Fill extends BrushBitmapFill
 {
 
 //vertical left, vertical right, horizontal top, horizontal bottom
+
 	public var scaleRect:Array<Int>;
+	public var debuglines:Bool = false;
 //todo smooth
 //todo stretch
-
 //todo cache sizes
 
 	override public function update( drawTarget:IOComponent )
 	{
 		var bitmapdata = images.get( drawTarget.state );
 
-		BitmapScaleSliceUtil.slice9( drawTarget.sprite, drawTarget.width, drawTarget.height, bitmapdata, scaleRect, smooth );
+		BitmapScaleSliceUtil.slice9( drawTarget.sprite, drawTarget.width, drawTarget.height, bitmapdata, scaleRect,
+		                             smooth, debuglines );
 	}
 
 	override public function destroy( ):Void
