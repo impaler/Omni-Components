ab694f5dd7b22bb1de529c12dc112e9db8f8f0e9
impaler <user123@server.fake>
2013-03-13 12:44:52 +1030

    radio button bugfix, don't disable if currently active

--- source/omni/components/gui/controls/RadioButton.hx
+++ source/omni/components/gui/controls/RadioButton.hx
@@ -1,9 +1,25 @@
 package omni.components.gui.controls;
 
+import omni.utils.signals.OSignalMouse;
 import omni.components.core.OToggleButton;
 
 class RadioButton extends OToggleButton
 {
+
+	public var currentSelection:Bool = false;
+
+	override public function handleMouseDown( ?e:OSignalMouse ):Void
+	{
+		if( !_value )
+		{
+			isDown = true;
+			_value = ! _value;
+			_state = getValueState( );
+			invalidate( );
+			onValueChange.dispatch( _value );
+			onChange.dispatch( this );
+		}
+	}
 
 //***********************************************************
 //                  Component Style
