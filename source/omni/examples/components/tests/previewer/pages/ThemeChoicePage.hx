41220a451fdb81d9e0c77c06c2ec19f5d3116020
impaler <user123@server.fake>
2013-08-12 21:59:58 +0930

    compile fix

--- source/omni/examples/components/tests/previewer/pages/ThemeChoicePage.hx
+++ source/omni/examples/components/tests/previewer/pages/ThemeChoicePage.hx
@@ -1,6 +1,6 @@
 package omni.examples.components.tests.previewer.pages;
 
-import omni.components.theme.softui.SoftUI;
+// import omni.components.theme.softui.SoftUI;
 import omni.components.theme.colors.Dark;
 import omni.components.theme.colors.Orange;
 import omni.components.core.OCore;
@@ -11,29 +11,29 @@ import omni.components.gui.controls.Button;
 class ThemeChoicePage extends ThemePage
 {
 
-    override public function createMembers ():Void
-    {
-        super.createMembers ();
+	override public function createMembers ():Void
+	{
+		super.createMembers ();
 
-        title = "Themes";
+		title = "Themes";
 
-        addTheme (Orange, "Orange");
-        addTheme (Dark, "Dark");
-        addTheme (SoftUI, "SoftUI");
-    }
+		addTheme (Orange, "Orange");
+		addTheme (Dark, "Dark");
+		// addTheme (SoftUI, "SoftUI");
+	}
 
-    public function addTheme (theme:Class<OTheme>, ?labelText:String):Void
-    {
-        var defaultButton = new Button();
-        defaultButton.text = labelText == null ? Type.getClassName (theme) : labelText;
-        defaultButton.onClick.add (
-            function (e:Dynamic):Void
-            {
-                if (Type.getClass(OCore.instance.defaultTheme) != theme)
-                    OCore.instance.changeDefaultTheme (theme);
-            }
-        );
-        add (defaultButton);
-    }
+	public function addTheme (theme:Class<OTheme>, ?labelText:String):Void
+	{
+		var defaultButton = new Button();
+		defaultButton.text = labelText == null ? Type.getClassName (theme) : labelText;
+		defaultButton.onClick.add (
+			function (e:Dynamic):Void
+			{
+				if (Type.getClass(OCore.instance.defaultTheme) != theme)
+					OCore.instance.changeDefaultTheme (theme);
+			}
+		);
+		add (defaultButton);
+	}
 
 }
